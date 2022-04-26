7918eb12d9df16c63a2b0db86785632634f28bf4
Artem Nilov <nilov1235@mail.ru>
2022-04-26 11:43:29 +0300

    change to eng

--- homework2lesson/homework2lesson.cpp
+++ homework2lesson/homework2lesson.cpp
@@ -9,8 +9,9 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     
-    //Задание 1
+    //Task 1
     double R0, R1, R2, R3;
+    std::cout << "Task 1 \n";
     std::cout << "Enter R1: \n";
     std::cin >> R1; 
     std::cout << "Enter R2: \n";
@@ -18,27 +19,29 @@ int main()
     std::cout << "Enter R3: \n";
     std::cin >> R3;
     R0 = 1 / ((1 / R1) + (1 / R2) + (1 / R3));
-    std::cout << "Значение сопротивления R0 равняется: " << R0 << std::endl;
+    std::cout << "R0: " << R0 << std::endl;
 
-    //Задание 2
+    //Task 2
+    std::cout << "Task 2\n";
     const double pi = 3.14159265359;
     double length,radius, S;
-    std::cout << "Введите длину окружности: ";
+    std::cout << "Enter length of the circle: ";
     std::cin >> length;
-    // Находим радиус
+    // radius
     radius = (length / pi) / 2;
-    //Находим и выводим площадь круга
-    std::cout << "Площадь круга равна: " << radius * radius * pi << std::endl;
+    //square
+    std::cout << "Square of cirlce: " << radius * radius * pi << std::endl;
 
-    //Задание 3
+    //Task 3
+    std::cout << "Task 3 \n";
     double v, t, a;
-    std::cout << "Введите скорость: ";
+    std::cout << "Speed: ";
     std::cin >> v;
-    std::cout << "Введите время движения: ";
+    std::cout << "Time: ";
     std::cin >> t; 
-    std::cout << "Введите ускорение: ";
+    std::cout << "Acceleration: ";
     std::cin >> a;
-    std::cout << "Пройденное расстояние при прямолинейном равноускоренном движении: " << v * t + (a * t * t) / 2 << std::endl;
+    std::cout << "Traveled distance: " << v * t + (a * t * t) / 2 << std::endl;
 
     return 0;
 }
